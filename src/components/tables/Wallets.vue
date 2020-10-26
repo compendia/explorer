35ad4548c89c7a7808ad88707793b875c4a5b11d
Dean van Dugteren <hello@dean.press>
2020-10-26 14:38:41 +0100

    fix(Wallets): fix getStaked

--- src/components/tables/Wallets.vue
+++ src/components/tables/Wallets.vue
@@ -160,7 +160,7 @@ export default class TableWalletsDesktop extends Vue {
   private getStaked(stakes: IStake[]) {
     let staked = BigNumber.ZERO;
     for (const stake of stakes) {
-      if (stake.status !== "released") {
+      if (!["canceled", "redeemed"].includes(stake.status)) {
         staked = staked.plus(BigNumber.make(stake.amount));
       }
     }
